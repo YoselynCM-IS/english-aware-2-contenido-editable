208cc373bd08560d6ecc96680c5c38fe62cfc9fb
lgonzalezru <65631466+lgonzalezru@users.noreply.github.com>
2020-07-07 23:19:41 -0500

    LGR

---
+++ send.php
@@ -0,0 +1,84 @@
+<?php
+
+/***librerias phpmailer**/
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+/* Exception class. */
+require 'PHPMailer/src/Exception.php';
+
+/* The main PHPMailer class. */
+require 'PHPMailer/src/PHPMailer.php';
+
+/* SMTP class, needed if you want to use SMTP. */
+require 'PHPMailer/src/SMTP.php';
+/**********/
+
+$imagen = $_POST["imagen"];
+$ea2_regGP = $_POST["ea2_regGP"];
+$ea2_regCA = $_POST["ea2_regCA"];
+$ea2_regND = $_POST["ea2_regND"];
+$ea2_regCD = $_POST["ea2_regCD"];
+$ea2_regNA = $_POST["ea2_regNA"];
+/*
+echo $imagen;
+echo "<br>";
+echo $ea2_regGP;
+echo "<br>";
+echo $ea2_regCA;
+echo "<br>";
+echo $ea2_regND;
+echo "<br>";
+echo $ea2_regCD;
+echo "<br>";
+echo $ea2_regNA;
+echo "<br>";
+//die("fin");
+**/
+$imagen = preg_replace('#^data:image/[^;]+;base64,#', '', $imagen); 
+$mensaje = '<b>English Aware 2</b><br><b>Student name: </b>'.$ea2_regNA.'<br><b>Group: </b> '.$ea2_regGP.'<br><b>Teacher: </b>'.$ea2_regND;
+
+$para = $ea2_regCD;
+$para2 = $ea2_regCA;
+$asunto = 'English Aware 2: Activity';				
+//Create a new PHPMailer instance
+$mail = new PHPMailer();
+$mail->IsSMTP();
+//Agregar la imagen
+$decode = base64_decode($imagen);
+$mail->addStringAttachment($decode, "Activity.png", "base64", "image/png");
+$mensaje .= '<br><img src="https://majesticeducacion.com.mx/nuevo/wp-content/uploads/2018/08/logo-header-majesticeducacion.png">';
+ 
+//Configuracion servidor mail
+
+$mail->From = "ebook@majesticeducationdigital.com"; //remitente
+$mail->FromName = "Majestic Education";//nombre remitente
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl'; //seguridad
+$mail->Host = "mail.majesticeducationdigital.com"; // servidor smtp
+$mail->Port = 465; //puerto
+$mail->Username ='ebook@majesticeducationdigital.com'; //nombre usuario
+$mail->Password = 'xAp3Rm9=xO+a'; //contraseña
+
+ 
+//Agregar destinatario
+$mail->AddAddress($para);
+$mail->AddAddress($para2);
+$mail->Subject = $asunto;
+$mail->IsHTML(true);
+$mail->Body = $mensaje;
+
+
+ 
+//Avisar si fue enviado o no y dirigir al index
+if ($mail->Send()) {
+    echo'<script type="text/javascript">
+           alert("Sent correctly");
+		   window.history.back();
+        </script>';
+} else {
+    echo'<script type="text/javascript">
+           alert("Not sent, try again");
+        </script>';
+}
+?>
